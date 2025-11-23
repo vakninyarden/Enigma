74c270de0b120c1849618409f4b6f910d5fb0f21
Yarden <yardenvaknin23@gmail.com>
2025-11-23 17:01:53 +0200

    implement rotor class

---
+++ Enigma_Machine/src/enigma/machine/components/Alphabet.java
@@ -0,0 +1,13 @@
+package enigma.machine.components;
+
+public class Alphabet {
+private final int size;
+
+    public Alphabet(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
--- Enigma_Machine/src/enigma/machine/components/Rotor.java
+++ Enigma_Machine/src/enigma/machine/components/Rotor.java
@@ -2,21 +2,70 @@ package enigma.machine.components;
 
 import enigma.machine.api.RotorDefinition;
 
-import java.util.Collection;
+import java.util.ArrayList;
+
 
 public class Rotor implements RotorDefinition {
 private final int id;
 private final int notchPosition;
-private final int currentPosition;
-private final Collection<Character> rightMapping;
-private final Collection<Character> leftMapping;
+private int currentPosition;
+private final int sizeOfAlphabet ;
+private final int originalPosition;
+private final ArrayList<Character> rightMapping;
+private final ArrayList<Character> leftMapping;
+
+    public Rotor(int id, int notchPosition, int currentPosition,
+                 ArrayList<Character> rightMapping, ArrayList<Character> leftMapping) {
+        this.sizeOfAlphabet = rightMapping.size();
+        this.originalPosition = currentPosition;
+        this.id = id;
+        this.notchPosition = notchPosition;
+        this.currentPosition = currentPosition;
+        this.rightMapping = rightMapping;
+        this.leftMapping = leftMapping;
+
+    }
+
     @Override
     public int getRotorId() {
-        return 0;
+        return id;
     }
 
     @Override
     public int getNotchIndex() {
-        return 0;
+        return notchPosition;
     }
+
+    @Override
+    public int rightToLeftMapping(int inputIndex) {
+        int shifted = (inputIndex + currentPosition) % sizeOfAlphabet;
+        char outChar = rightMapping.get(shifted);
+        int mapped = leftMapping.indexOf(outChar);
+        return (mapped - currentPosition + sizeOfAlphabet) % sizeOfAlphabet;
+    }
+
+    @Override
+    public int leftToRightMapping(int indexInRotor) {
+        int shifted = (indexInRotor + currentPosition) % sizeOfAlphabet;
+        char outChar = leftMapping.get(shifted);
+        int mapped = rightMapping.indexOf(outChar);
+        return (mapped - currentPosition + sizeOfAlphabet) % sizeOfAlphabet;
+    }
+
+    @Override
+    public boolean atNotch() {
+        return currentPosition == notchPosition;
+    }
+
+    @Override
+    public void step() {
+        currentPosition = (currentPosition + 1) % sizeOfAlphabet;
+    }
+
+    @Override
+    public void reset() {
+        currentPosition = originalPosition;
+    }
+
+
 }
